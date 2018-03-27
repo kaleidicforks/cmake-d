c41d87630e294ff624c8266c79667dbb2c38ff10
Laeeth Isharc <laeeth@kaleidic.io>
2018-03-27 09:35:52 +0000

    change phobos library so it picks up appropriate one for compiler - dmd/ldc/gdc

--- cmake-d/FindPhobos.cmake
+++ cmake-d/FindPhobos.cmake
@@ -1,5 +1,15 @@
 find_path(PHOBOS_INCLUDE_DIR std/file.d)
-find_library(PHOBOS_LIBRARY phobos2)
+
+if("${CMAKE_D_COMPILER_ID}" STREQUAL "DigitalMars")
+	find_library(PHOBOS_LIBRARY phobos2)
+elseif("${CMAKE_D_COMPILER_ID}" STREQUAL "LDC")
+	find_library(PHOBOS_LIBRARY phobos2-ldc)
+elseif("${CMAKE_D_COMPILER_ID}" STREQUAL "GDC")
+	find_library(PHOBOS_LIBRARY gphobos)
+else
+	message(FATAL_ERROR "Unsupported D compiler: ${CMAKE_D_COMPILER_ID}")
+endif()
+
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Phobos DEFAULT_MSG PHOBOS_LIBRARY PHOBOS_INCLUDE_DIR)
